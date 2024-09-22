535a6c65c74746af054aee284374f1e67cf790e7
ringoXD <ringoxd@ringoxd.dev>
2024-09-22 23:18:41 +0900

    進捗だけ

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,20 +7,32 @@ export default defineConfig({
 	site: 'https://djs-docs.pages.dev',
 	integrations: [
 		starlight({
-			title: 'Discord.js Tutorial',
+			title: 'Discord.js 日本語ガイド',
+			locales: {
+				root: {
+					label: '日本語',
+					lang: 'ja',
+				},
+			},
 			social: {
 				github: 'https://github.com/ringo360/djs-docs',
 			},
 			editLink: {
 				baseUrl: 'https://github.com/ringo360/djs-docs/edit/master/',
 			},
+			disable404Route: true,
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Hello discord.js!',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Discord.jsとは', link: '/about/introduction' },
+						{ label: 'はじめましょう！', link: '/about/getting-started' },
 					],
+					// autogenerate: { directory: 'about' },
+				},
+				{
+					label: 'チュートリアル',
+					autogenerate: { directory: 'tutorial' },
 				},
 				{
 					label: 'Reference',
---
+++ src/content/docs/about/introduction.md
@@ -0,0 +1,25 @@
+---
+title: Discord.jsとは?
+description: まずは、Discord.jsについて知りましょう。
+---
+
+discord.js は、Node.js のモジュールの一つです。Discord の API を非常に簡単なコードで操作できます。大半の BOT は discord.js を利用して稼働しており、情報量も他のモジュールに比べてはるかに多いです。
+
+このドキュメントでは、discord.js を使って BOT を作ろうとしているあなたをサポートするための手助けをします。
+
+## 始める前に...
+
+Discord.js で BOT を作ろうとしているあなたの意志は非常に素晴らしいです。
+いかし、あなたはいくつかの知識を予め身に着けている必要があります。
+
+- JavaScript(+Node.js) に関する基本的な知識
+- npm 等のパッケージマネージャーの使い方
+- Discord に関する最低限の知識
+
+しかし、知識がない場合でも心配は要りません！大抵の問題は「[ググる](https://google.com/)」ことで解決できます。
+困ったら調べる。この繰り返しはあなたを確実に成長させます。
+
+また、[MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript)は JavaScript の基本的な事を学ぶ上で非常に役立ちます。
+[JavaScript.info](https://javascript.info/)もその一つです。
+
+JavaScript の基礎を少しだけ学んで、自信がついたら早速始めてください！
